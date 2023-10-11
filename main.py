c96c1271d07e1b794b0fc5200f49cfba949fc15c
TSBread <2638318806@qq.com>
2023-10-11 22:52:26 +0800

    1

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import os
 import json
+import base64
 import warnings
 from urllib.request import urlopen
 from urllib.request import Request
 
 import requests
 
+headers = {'User-Agent': 'Mozilla/5.0',
+           'Authorization': 'token ' + os.environ.get('TOKEN'),
+           'Accept': 'application/vnd.github.v3+json'}
 
-def delete_commit_value():
-    repo_owner = 'TSBread'
-    repo_name = 'minecraft-global-scoreboard-template-Packing'
-    
+
+def delete_commit_value(repo_owner, repo_name):
     url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/contents'
-    headers = {'User-Agent': 'Mozilla/5.0',
-               'Authorization': 'token ' + os.environ.get('TOKEN'),
-               'Accept': 'application/vnd.github.v3+json'}
-    req = Request(url, headers=headers)
-    response = urlopen(req).read()
-    result = json.loads(response.decode())
+    result = json.loads(urlopen(Request(url, headers=headers)).read().decode())
     for i in result:
         if i["name"] == "value":
-            print(i['sha'])
-            data = {"message": "Merge Successful", "sha": i['sha']}
+            data = {"message": "合并成功", "sha": i['sha']}
             warnings.filterwarnings('ignore')
-            del_rep = requests.delete(url + '/value', data=json.dumps(data), headers=headers, verify=False)
-            print(del_rep.text)
+            requests.delete(url + '/value', data=json.dumps(data), headers=headers, verify=False)
+            c_url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/git/blobs/' + i['sha']
+            content = json.loads(urlopen(Request(c_url, headers=headers)).read().decode())
+            return bytes.decode(base64.b64decode(content['content']))
 
 
 if __name__ == '__main__':
-    delete_commit_value()
+    owner = 'TSBread'
+    name = 'minecraft-global-scoreboard-template-Packing'
+    delete_commit_value(owner, name)
