ab3dbf70a272f3aa5d60b8a8adb32bb54811a331
TSBread <2638318806@qq.com>
2023-10-11 23:25:17 +0800

    1

--- main.py
+++ main.py
@@ -12,20 +12,30 @@ headers = {'User-Agent': 'Mozilla/5.0',
            'Accept': 'application/vnd.github.v3+json'}
 
 
-def delete_commit_value(repo_owner, repo_name):
+def get_repo_content(repo_owner, repo_name):
     url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/contents'
-    result = json.loads(urlopen(Request(url, headers=headers)).read().decode())
-    for i in result:
+    return json.loads(urlopen(Request(url, headers=headers)).read().decode())
+
+
+def get_player_update_info(repo_owner, repo_name, repo):
+    for i in repo:
         if i["name"] == "value.mgst":
-            warnings.filterwarnings('ignore')
-            data = {"message": "合并成功", "sha": i['sha']}
-            requests.delete(url + '/value.mgst', data=json.dumps(data), headers=headers, verify=False)
-            c_url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/git/blobs/' + i['sha']
-            content = json.loads(urlopen(Request(c_url, headers=headers)).read().decode())
+            url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/git/blobs/' + i['sha']
+            content = json.loads(urlopen(Request(url, headers=headers)).read().decode())
             return bytes.decode(base64.b64decode(content['content']))
+
+
+def delete_player_update_info(repo_owner, repo_name, repo):
+    url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/contents'
+    for i in repo:
+        warnings.filterwarnings('ignore')
+        data = {"message": "合并成功", "sha": i['sha']}
+        requests.delete(url + '/value.mgst', data=json.dumps(data), headers=headers, verify=False)
 
 
 if __name__ == '__main__':
     owner = 'TSBread'
-    name = 'minecraft-global-scoreboard-template-Packing'
-    delete_commit_value(owner, name)
+    name = 'minecraft-global-scoreboard-template-Packing'  # minecraft-global-scoreboard-template-Packing
+    repo_content = get_repo_content(owner, name)
+    print(get_player_update_info(owner, name, repo_content))
+    delete_player_update_info(owner, name, repo_content)
