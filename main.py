4bfa7be689e9d90f19ca35bcd50e6470c3aed705
TSBread <2638318806@qq.com>
2023-10-11 12:41:12 +0800

    1

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ import requests
 
 
 def delete_commit_value():
-    url = 'https://api.github.com/repos/TSBread/minecraft-global-scoreboard-template-Packing/contents'
+    repo_owner = 'TSBread'
+    repo_name = 'minecraft-global-scoreboard-template-Packing'
+    
+    url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/contents'
     headers = {'User-Agent': 'Mozilla/5.0',
                'Authorization': 'token ' + os.environ.get('TOKEN'),
                'Accept': 'application/vnd.github.v3+json'}
     req = Request(url, headers=headers)
     response = urlopen(req).read()
     result = json.loads(response.decode())
-    # print(result)
     for i in result:
         if i["name"] == "value":
             print(i['sha'])
-            data = {"message": "delete a file", "sha": i['sha']}
+            data = {"message": "Merge Successful", "sha": i['sha']}
             warnings.filterwarnings('ignore')
             del_rep = requests.delete(url + '/value', data=json.dumps(data), headers=headers, verify=False)
             print(del_rep.text)
