595548382bc901164f80cc9d196790f626671e9a
TSBread <2638318806@qq.com>
2023-10-13 20:09:34 +0800

    1

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ def delete_file_from_repo(repo_owner, repo_name, repo, file_name):
 def zip_files_in_buffer(path):
     buffer = io.BytesIO()
     zfile = zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED)
+    print(os.walk(path))
     for root, dirs, files in os.walk(path):
         relative_root = '' if root == path else root.replace(path, '') + os.sep
         for filename in files:
@@ -94,7 +95,7 @@ if __name__ == '__main__':
     data = get_player_update_info(owner, name, repo_content)
     merge_file_data(owner, name, data)
     with open(packing_name, 'wb') as f:
-        print(zip_files_in_buffer('saves').getbuffer())
+        f.write(zip_files_in_buffer('saves').getbuffer())
     delete_file_from_repo(owner, name, repo_content, packing_name)
     update_file_to_repo(owner, name, packing_name)
 
